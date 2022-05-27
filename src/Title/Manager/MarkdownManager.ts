a1412695c61ff4d2c418f6f4cdca8d6395a89e13
snezhig <sneqastk@gmail.com>
2022-05-28 00:00:03 +0400

    Create class for markdown, realize base logic with tests. Not completed

--- src/Title/Manager/MarkdownManager.ts
+++ src/Title/Manager/MarkdownManager.ts
@@ -1,9 +1,24 @@
 import Manager from "./Manager";
-import {TAbstractFile} from "obsidian";
+import {MarkdownViewExt, TAbstractFile, Workspace} from "obsidian";
+import {Leaves} from "../../enum";
+import Resolver from "../Resolver/Resolver";
 
-export default class MarkdownManager implements Manager{
+export default class MarkdownManager implements Manager {
+    private original = new Map<string, string>();
+
+    constructor(
+        private workspace: Workspace,
+        private resolver: Resolver
+    ) {
+    }
+
     private enabled: boolean = false;
+
     disable(): void {
+        for(const leaf of this.workspace.getLeavesOfType(Leaves.MD)){
+            this.setTitle(leaf.view as MarkdownViewExt, null);
+        }
+        this.original.clear();
         this.enabled = false;
     }
 
@@ -15,8 +30,33 @@ export default class MarkdownManager implements Manager{
         return this.enabled;
     }
 
-    update(abstract?: TAbstractFile | null): Promise<boolean> {
-        return Promise.resolve(false);
+    async update(abstract?: TAbstractFile | null): Promise<boolean> {
+        const leaves = this.workspace.getLeavesOfType(Leaves.MD);
+        const promises = [];
+
+        for (const leaf of leaves) {
+            const view: MarkdownViewExt = (leaf.view as MarkdownViewExt);
+            if (view.file.path === abstract?.path) {
+                this.setTitle(view, await this.resolver.resolve(view.file));
+            } else if (!abstract) {
+                promises.push(this.resolver.resolve(view.file).then(r => [view, r]));
+            }
+        }
+
+        for (const [view, title] of await Promise.all(promises)) {
+            this.setTitle(view as MarkdownViewExt, title as string | null);
+        }
+
+        return Promise.resolve(true);
+    }
+
+    private setTitle(view: MarkdownViewExt, title: string | null): void {
+        if (title) {
+            this.original.set(view.file.path, view.titleEl.innerText);
+            view.titleEl.innerText = title;
+        } else if (this.original.has(view.file.path)) {
+            view.titleEl.innerText = this.original.get(view.file.path);
+        }
     }
 
 }
--- test/unit/Title/Manager/MarkdownManager.spec.ts
+++ test/unit/Title/Manager/MarkdownManager.spec.ts
@@ -1,8 +1,12 @@
 import Manager from "../../../../src/Title/Manager/Manager";
 import MarkdownManager from "../../../../src/Title/Manager/MarkdownManager";
-import {MarkdownViewExt, TFile, Workspace} from "obsidian";
+import {MarkdownViewExt, TFile, Workspace, WorkspaceLeaf} from "obsidian";
+import Resolver from "../../../../src/Title/Resolver/Resolver"
 
-let manager: Manager = new MarkdownManager();
+let manager: Manager = new MarkdownManager(
+    Object.create(Workspace.prototype),
+    Object.create(Resolver.prototype)
+);
 const createView = (name: string) => {
     const titleEl = new class {
         constructor(private text: string) {
@@ -29,8 +33,11 @@ const views: { [k: string]: MarkdownViewExt } = {
     bar: createView('bar'),
 }
 const mocks = {
-    foo: {innerText: jest.spyOn<HTMLDivElement, any>(views.foo.titleEl, 'innerText', 'get')},
-    bar: {innerText: jest.spyOn<HTMLDivElement, any>(views.bar.titleEl, 'innerText', 'get')},
+    foo: {innerText: jest.spyOn<HTMLDivElement, any>(views.foo.titleEl, 'innerText', 'set')},
+    bar: {innerText: jest.spyOn<HTMLDivElement, any>(views.bar.titleEl, 'innerText', 'set')},
+    resolver: {
+        resolve: jest.spyOn<Resolver, 'resolve'>(Resolver.prototype, 'resolve')
+    },
     workspace: {
         getLeavesOfType: jest.spyOn<Workspace, 'getLeavesOfType'>(Workspace.prototype, 'getLeavesOfType')
     }
@@ -53,6 +60,47 @@ describe('Markdown leaf test', () => {
     });
 
     describe('Plugin with leaves', () => {
+        beforeAll(() => {
+            manager.enable();
+        })
+        describe('Test Foo view', () => {
+            beforeAll(() => {
+                mocks.workspace.getLeavesOfType.mockReturnValue([{view: views.foo} as unknown as WorkspaceLeaf]);
+            })
+            beforeEach(() => {
+                mocks.foo.innerText.mockClear();
+            })
+            test('Has been changed', async () => {
+                const title = 'new_title';
+                mocks.resolver.resolve.mockResolvedValueOnce(title);
+                await manager.update();
+                expect(mocks.foo.innerText).toHaveBeenNthCalledWith(1, title);
+            })
 
+            test('Has not been changed', async () => {
+                await manager.update(views.bar.file);
+                expect(mocks.foo.innerText).not.toHaveBeenCalled();
+            })
+
+            test('Has been restores because of null', async () => {
+                mocks.resolver.resolve.mockResolvedValueOnce(null);
+                await manager.update()
+                expect(mocks.foo.innerText).toHaveBeenNthCalledWith(1, 'foo');
+            })
+
+            test('Has been changed and restored because of disable', async () => {
+                const title = 'test';
+                mocks.resolver.resolve.mockResolvedValueOnce(title);
+                await manager.update();
+                expect(mocks.foo.innerText).toHaveBeenNthCalledWith(1, title);
+                manager.disable();
+                expect(mocks.foo.innerText).toHaveBeenNthCalledWith(2, 'foo');
+                expect(mocks.foo.innerText).toHaveBeenCalledTimes(2);
+            })
+
+            afterAll(() => {
+                mocks.workspace.getLeavesOfType.mockReset();
+            })
+        })
     })
 })
