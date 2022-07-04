3df4aa917b56a1f568d47802b95e9778b233cce7
snezhig <sneqastk@gmail.com>
2022-07-04 19:33:29 +0300

    Add base of QuickSwitcher

--- src/Title/Manager/QuickSwitcher.ts
+++ src/Title/Manager/QuickSwitcher.ts
@@ -1,53 +1,79 @@
 // Example
-import {Modal, SuggestModal} from "obsidian";
+import {SuggestModalExt, SuggestModalChooser, TAbstractFile, SuggestModal, TFile} from "obsidian";
 import FunctionReplacer from "../../Utils/FunctionReplacer";
+import Manager from "./Manager";
+import Resolver from "../Resolver/Resolver";
 
-const S = this;
-await (async () => {
-    //@ts-ignore
-    global.a = Modal;
-    // const c = Modal.prototype.open;
-    const test = {
-        m: null,
-        r: null,
+
+type Replacers = {
+    modal?: FunctionReplacer<SuggestModalExt<any>, 'open', QuickSwitcher>,
+    chooser?: FunctionReplacer<SuggestModalChooser<any>, 'setSuggestions', QuickSwitcher>
+}
+
+export default class QuickSwitcher implements Manager {
+    private replacers: Replacers = {};
+
+    private state: boolean;
+
+    constructor(
+        private resolver: Resolver
+    ) {
+        this.replacers.modal = new FunctionReplacer<SuggestModalExt<any>, "open", QuickSwitcher>(
+            SuggestModal.prototype, 'open', this, function (self, defaultArgs, vanilla) {
+                self.createChooserReplacer.call(self, this);
+                return vanilla.call(this, ...defaultArgs);
+            }
+        )
+        this.replacers.modal.enable()
     }
-    test.m = new FunctionReplacer<SuggestModal<any>, 'open', () => void>(
-        SuggestModal.prototype,
-        'open',
-        (c) => {
-            test.r = new FunctionReplacer(c, 'setSuggestions', [], function (args, defaultArgs, vanilla) {
-                console.log(defaultArgs);
-                const i = [];
-                for (const item of defaultArgs[0]){
-                    // if(item.type === 'file'){
-                    item.alias = S.resolver.getResolved(item.file);
-                    item.type = 'alias'
-                    i.push(item)
-                    // }
-                }
-                console.log(i);
-                vanilla.call(this, i);
 
-            });
-            console.log(test)
-            test.r.enable();
-        },
-        function (args, defaultArgs, vanilla)
-        {
-            console.log('open')
-            console.log(defaultArgs);
-            args(this.chooser);
-            console.log(this);
-            vanilla.call(this);
-            console.log(args);
-            test.m.disable();
+
+    private createChooserReplacer(modal: SuggestModalExt<any>): void {
+        console.log(modal);
+        if (this.replacers.chooser) {
+            return;
         }
-    )
+        if (typeof modal?.chooser?.setSuggestions !== "function") {
+            return;
+        }
+        this.replacers.chooser = new FunctionReplacer<SuggestModalChooser<any>, "setSuggestions", QuickSwitcher>(
+            modal.chooser.__proto__,
+            'setSuggestions',
+            this,
+            //TODO: make separate function
+            function (self, defaultArgs, vanilla) {
+                self.replacers.modal.disable();
+                //TODO make more readable
+                if (defaultArgs?.[0]?.[0]?.type === 'file') {
+                    //TOOO: resolve new title
+                    console.log(defaultArgs);
+                }
+                return vanilla.call(this, ...defaultArgs);
+            }
+        )
+        this.replacers.chooser.enable();
 
-    test.m.enable();
+    }
 
-    // Modal.prototype.open = function () {
-    //     console.log(this);
-    //     c.call(this)
-    // }
-})();
+    disable(): Promise<void> | void {
+        this.replacers.modal.disable();
+        this.replacers?.chooser.disable();
+        this.state = false;
+    }
+
+    enable(): Promise<void> | void {
+        if (!this.replacers?.chooser.enable()) {
+            this.replacers.modal.enable()
+        }
+
+        this.state = true;
+    }
+
+    isEnabled(): boolean {
+        return this.state;
+    }
+
+    update(abstract?: TAbstractFile | null): Promise<boolean> {
+        return Promise.resolve(false);
+    }
+}
