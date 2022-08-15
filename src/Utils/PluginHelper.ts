d8d182fbfa4f79f65fb1b8eb32934461438f4233
snezhig <sneqastk@gmail.com>
2022-08-15 14:07:14 +0300

    Fix tests

---
+++ src/Utils/PluginHelper.ts
@@ -0,0 +1,26 @@
+import {SettingsType} from "@src/Settings/SettingsType";
+
+export default class PluginHelper{
+    public static createDefaultSettings(): SettingsType{
+        return {
+            template: '',
+            managers: {
+                explorer: false,
+                graph: false,
+                header: false,
+                quick_switcher: false
+            },
+            rules: {
+                paths: {
+                    mode: 'black',
+                    values: []
+                },
+                delimiter: {
+                    enabled: false,
+                    value: ''
+                }
+            },
+            debug: false
+        }
+    }
+}
