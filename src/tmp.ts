f67cda8221acb5eda04f70d5c3538ae2622c47e6
snezhig <sneqastk@gmail.com>
2022-08-21 16:36:16 +0300

    Remove old files

--- src/tmp.ts
+++
@@ -1,7 +0,0 @@
-import Container from '../config/inversify.config';
-import ResolverInterface from "@src/Interfaces/ResolverInterface";
-import SI from "@config/inversify.types";
-
-const resolver = Container.getNamed<ResolverInterface>(SI.resolver, 'sync');
-resolver.resolve('/test.md');
-console.log(resolver);
