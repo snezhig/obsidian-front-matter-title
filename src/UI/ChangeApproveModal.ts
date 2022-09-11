e55640178973475af3830b02af87951713c052f8
snezhig <sneqastk@gmail.com>
2022-09-11 10:40:31 +0300

    Complete modal

--- src/UI/ChangeApproveModal.ts
+++ src/UI/ChangeApproveModal.ts
@@ -1,32 +1,71 @@
-import {App, Modal, Setting} from "obsidian";
-import {inject} from "inversify";
+import { App, Modal, Setting } from "obsidian";
+import { inject, injectable } from "inversify";
 import SI from "@config/inversify.types";
 import DispatcherInterface from "@src/Components/EventDispatcher/Interfaces/DispatcherInterface";
-import {AppEvents} from "@src/Types";
+import { AppEvents } from "@src/Types";
 
-export default class ChangeApproveModal extends Modal {
-    constructor(
-        @inject(SI["obsidian:app"])
-            app: App,
-        @inject(SI.dispatcher)
-            dispatcher: DispatcherInterface<AppEvents>
-    ) {
-        super(app);
+@injectable()
+export default class ChangeApproveModal {
+  private instances: WeakMap<
+    Modal,
+    {
+      path: string;
+      changes: [string, string][];
+      approve: (approved: boolean) => void;
     }
+  > = new Map();
+  constructor(
+    @inject(SI["obsidian:app"])
+    private app: App,
+    @inject(SI.dispatcher)
+    dispatcher: DispatcherInterface<AppEvents>
+  ) {}
+  create(
+    path: string,
+    changes: [string, string][],
+    approve: (approved: boolean) => void
+  ): Modal {
+    const modal = new Modal(this.app);
+    modal.onOpen = () => this.onOpen(modal);
+    modal.onClose = () => this.approve(modal, false);
+    this.instances.set(modal, { path, changes, approve });
+    return modal;
+  }
 
-    onOpen() {
-        this.contentEl.createEl('h4', {text: "Approving file's content changes:"});
-        const div = this.contentEl.createDiv()
-        this.contentEl.createEl('div', {}, (d) => {
-            new Setting(div)
-                .addButton(c => c.setButtonText("Approve").setClass('mod-cta')).settingEl.className = ''
-            new Setting(div)
-                .addButton(c => c.setButtonText("Approve everytime").setClass('mod-cta')).settingEl.className = ''
-            new Setting(div)
-                .addButton(c => c.setButtonText("Discard").setClass('mod-cta')).settingEl.className = ''
-
-        })
-        div.style.display = 'flex'
-        div.style.justifyContent = 'space-between'
+  private approve(modal: Modal, approved: boolean): void {
+    if (this.instances.has(modal)) {
+      this.instances.get(modal).approve(approved);
+      this.instances.delete(modal);
     }
+  }
+
+  private onOpen(modal: Modal) {
+    if (!this.instances.has(modal)) {
+      modal.close();
+    }
+    const { path, changes } = this.instances.get(modal);
+    modal.contentEl.createEl("h4", {
+      text: "Approving file's content changes:",
+    });
+    const div = modal.contentEl.createDiv();
+    modal.contentEl.createEl("div", {}, (d) => {
+      new Setting(div).addButton((c) =>
+        c
+          .setButtonText("Approve")
+          .setClass("mod-cta")
+          .onClick(() => {
+            this.approve(modal, true);
+            modal.close();
+          })
+      ).settingEl.className = "";
+      new Setting(div).addButton((c) =>
+        c
+          .setButtonText("Discard")
+          .setClass("mod-cta")
+          .onClick(() => modal.close())
+      ).settingEl.className = "";
+    });
+    div.style.display = "flex";
+    div.style.justifyContent = "space-around";
+  }
 }
