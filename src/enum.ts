4a6bfea0c2d2c1791a35f323d451d0e59137314a
snezhig <sneqastk@gmail.com>
2022-09-11 10:42:11 +0300

    Update settings structure

--- src/enum.ts
+++ src/enum.ts
@@ -1,17 +1,19 @@
 export enum Leaves {
-    FE = 'file-explorer',
-    G = 'graph',
-    LG = 'localgraph',
-    MD = 'markdown',
+  FE = "file-explorer",
+  G = "graph",
+  LG = "localgraph",
+  MD = "markdown",
 }
 
 export enum Manager {
-    Explorer = 'explorer',
-    Graph = 'graph',
-    Header = 'header',
-    QuickSwitcher = 'quick_switcher'
+  Explorer = "explorer",
+  Graph = "graph",
+  Header = "header",
+  QuickSwitcher = "quick_switcher",
+  FilteNoteLink = "file_note_link",
 }
 
 export enum Feature {
-    ExplorerSort = 'explorer:sort'
+  ExplorerSort = "explorer:sort",
+  FileNoteLink = "file:note:link",
 }
