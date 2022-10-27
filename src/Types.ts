7ab480acfaf051e85334379d128932fba173a814
snezhig <sneqastk@gmail.com>
2022-10-27 18:46:30 +0300

    Add new Event

--- src/Types.ts
+++ src/Types.ts
@@ -1,7 +1,9 @@
 import { Feature, Manager } from "@src/enum";
 import { NoteLink } from "./Utils/FileNoteLinkService";
+import {CachedMetadata, TFile} from "obsidian";
 
 export type AppEvents = {
+    "metadata:cache:changed": {file: TFile, data: string, cache: CachedMetadata};
     "templates:changed": { old: string[]; new: string[] };
     "manager:update": { id: Manager; result: boolean };
     "feature:state:changed": { id: Feature; enabled: boolean };
