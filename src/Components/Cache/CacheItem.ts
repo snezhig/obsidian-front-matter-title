335d9a370655d0037bf9342be4614ceddae8f799
snezhig <sneqastk@gmail.com>
2023-07-05 19:28:39 +0300

    Fix

--- src/Components/Cache/CacheItem.ts
+++ src/Components/Cache/CacheItem.ts
@@ -1,11 +1,7 @@
 import CacheItemInterface from "./CacheItemInterface";
 
 export default class CacheItem<T> implements CacheItemInterface<T> {
-    constructor(
-        private _isHit: boolean,
-        private value: T,
-        private key: string
-    ) {}
+    constructor(private _isHit: boolean, private value: T, private key: string) {}
 
     get(): T {
         return this.value;
