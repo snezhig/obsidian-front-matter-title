ac7091cfab11a4d9781d6e5b02d6127c8d706886
snezhig <sneqastk@gmail.com>
2023-07-19 17:03:22 +0300

    Create wrapper, change search manager and backlink

---
+++ src/Utils/ResultDomWrapper.ts
@@ -0,0 +1,41 @@
+import { SearchDOM, TFile } from "obsidian";
+import { ResolverInterface } from "../Resolver/Interfaces";
+import FunctionReplacer from "./FunctionReplacer";
+
+export default class ResultDomWrapper {
+    private replacer: FunctionReplacer<SearchDOM, "addResult", ResultDomWrapper> = null;
+    constructor(private resolver: ResolverInterface = null, dom: SearchDOM) {
+        this.replacer = FunctionReplacer.create(dom, "addResult", this, (self, defaultArgs, vanilla) => {
+            const c = vanilla.call(dom, ...defaultArgs);
+            const file = defaultArgs[0];
+            self.processLookupItem(file, c);
+            return c;
+        });
+        this.replacer.enable();
+        this.process();
+    }
+
+    public disable(): void {
+        this.replacer.disable();
+        this.replacer = null;
+    }
+
+    private process(): void {
+        const lookup = this.replacer?.getTarget()?.resultDomLookup ?? new Map();
+        console.log(lookup);
+        for (const [file, item] of lookup) {
+            this.processLookupItem(file, item);
+        }
+    }
+    private processLookupItem(file: TFile, item: any): void {
+        const restore = !this?.replacer?.isEnabled();
+        if (file.extension !== "md") {
+            return;
+        }
+        const node = item.containerEl.firstElementChild.find(".tree-item-inner");
+        const text = (restore ? null : this.resolver.resolve(file.path)) ?? file.basename;
+        if (node.getText() !== text) {
+            node.setText(text);
+        }
+    }
+}
---
+++ test/unit/Feature/Search/SearchFeaturer.spec.ts
@@ -0,0 +1,28 @@
+import { mock } from "jest-mock-extended";
+import ObsidianFacade from "@src/Obsidian/ObsidianFacade";
+import LoggerInterface from "@src/Components/Debug/LoggerInterface";
+import { Feature, Leaves } from "@src/Enum";
+import { SearchPluginView, SearchDOM, TFile } from "obsidian";
+import SearchFeature from "@src/Feature/Search/SearchFeature";
+import { ResolverInterface } from "@src/Resolver/Interfaces";
+import FeatureService from "../../../../src/Feature/FeatureService";
+import SearchDomWrapperService from "../../../../src/Utils/SearchDomWrapperService";
+
+const addResult = jest.fn();
+const mockDom = mock<SearchDOM>({ addResult }, { deep: true });
+const mockView = mock<SearchPluginView>({ dom: mockDom });
+const mockFacade = mock<ObsidianFacade>();
+const mockResolver = mock<ResolverInterface>();
+const mockFactory = mock<FeatureService>();
+const mockService = mock<SearchDomWrapperService>();
+mockFactory.createResolver.mockReturnValueOnce(mockResolver);
+const feature = new SearchFeature(mockFacade, mock<LoggerInterface>(), mockService, mockFactory);
+
+test(`Should return ${Feature.Search}`, () => expect(feature.getId()).toEqual(Feature.Search));
+
+describe("Search feature test", () => {
+    beforeEach(() => mockFacade.getViewsOfType.mockClear());
+    test("Should throw exception because there is no leave", () => {
+        expect(() => feature.enable()).toThrow(`View of ${Leaves.S} not found`);
+    });
+});
